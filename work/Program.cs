55c8999e219cd8e4abe8a1dbbe86877e172420f5
Ivan Maltsev <imaltsev2018i@gmail.com>
2022-10-04 18:56:42 +0300

    data input+

--- work/Program.cs
+++ work/Program.cs
@@ -1,2 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+void input(string h)
+{
+    Console.WriteLine("Введите строку: ");
+    string o = Console.ReadLine();
+    if (o != "")
+        {
+            if (o != "")
+            {
+                if (o.length<4)
+                {
+                    h = h + " " + search(o);
+                }
+            }
+            input(h);
+        }
+    else
+    {
+        Console.Write(h + " ");
+    }
+}
+string z = "";
+input(z);
